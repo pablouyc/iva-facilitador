a6b6869f1d8126f0d95ef7e2e7f5b5c234826b51
pablouyc <pablo@uycgroup.com>
2025-08-12 16:03:09 -0600

    chore: registrar QuickBooksApi en DI

--- Program.cs
+++ Program.cs
@@ -3,15 +3,23 @@ using Microsoft.AspNetCore.Localization;
 using IvaFacilitador.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Config
 builder.Services.Configure<IntuitOAuthSettings>(builder.Configuration.GetSection("IntuitAuth"));
+
+// MVC/Razor + HttpClient
 builder.Services.AddRazorPages();
 builder.Services.AddHttpClient();
+
+// Stores y servicios
 builder.Services.AddSingleton<ICompanyStore, FileCompanyStore>();
 builder.Services.AddSingleton<ITokenStore, FileTokenStore>();
 builder.Services.AddScoped<IQuickBooksAuth, QuickBooksAuth>();
+builder.Services.AddScoped<IQuickBooksApi, QuickBooksApi>(); // ← NUEVO
 
 var app = builder.Build();
 
+// Cultura es-CR
 var supportedCultures = new[] { new CultureInfo("es-CR") };
 app.UseRequestLocalization(new RequestLocalizationOptions{
     DefaultRequestCulture = new RequestCulture("es-CR"),
