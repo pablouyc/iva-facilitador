351f1a832d2b93f7c7512ad41fb953aaf784b457
pablouyc <pablo@uycgroup.com>
2025-09-17 19:33:15 -0600

    FUN-00: Scaffolding Payroll (área, rutas, feature flag, roles y options) sin tocar lo existente

---
+++ Domain/Payroll/PayrollRoles.cs
@@ -0,0 +1,10 @@
+namespace IvaFacilitador.Domain.Payroll
+{
+    public static class PayrollRoles
+    {
+        public const string Admin    = "Admin";
+        public const string RRHH     = "RRHH";
+        public const string Contador = "Contador";
+        public const string Lectura  = "Lectura";
+    }
+}
