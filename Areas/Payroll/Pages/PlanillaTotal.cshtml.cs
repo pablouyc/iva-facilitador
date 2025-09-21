76be0ac744f916a5b484f7b35af857af554448fa
pablouyc <pablo@uycgroup.com>
2025-09-21 13:00:16 -0600

    Payroll: asegura enlace 'Planilla total' en el Topbar y marca pestaña activa; incluye página/mock si existe.

---
+++ Areas/Payroll/Pages/PlanillaTotal.cshtml.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IvaFacilitador.Areas.Payroll.Pages
+{
+    public class PlanillaTotalModel : PageModel
+    {
+        public void OnGet() { }
+    }
+}
