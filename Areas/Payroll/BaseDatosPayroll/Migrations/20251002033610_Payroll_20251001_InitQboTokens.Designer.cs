3a62808dfaa565b3c04f70b7114e4a98e16a6ae2
pablouyc <pablo@uycgroup.com>
2025-10-01 21:39:11 -0600

    Payroll: add EF migration Payroll_20251001_InitQboTokens (+ QBO auth wiring)

--- Areas/Payroll/BaseDatosPayroll/Migrations/20251002033610_Payroll_20251001_InitQboTokens.Designer.cs
+++ Areas/Payroll/BaseDatosPayroll/Migrations/20251002033610_Payroll_20251001_InitQboTokens.Designer.cs
@@ -3,16 +3,19 @@ using System;
 using IvaFacilitador.Areas.Payroll.BaseDatosPayroll;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace IvaFacilitador.Areas.Payroll.BaseDatosPayroll.Migraciones
+namespace IvaFacilitador.Areas.Payroll.BaseDatosPayroll.Migrations
 {
     [DbContext(typeof(PayrollDbContext))]
-    partial class PayrollDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251002033610_Payroll_20251001_InitQboTokens")]
+    partial class Payroll_20251001_InitQboTokens
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.9");
---
+++ Areas/Payroll/BaseDatosPayroll/Migrations/20251002033610_Payroll_20251001_InitQboTokens.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IvaFacilitador.Areas.Payroll.BaseDatosPayroll.Migrations
+{
+    /// <inheritdoc />
+    public partial class Payroll_20251001_InitQboTokens : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PayrollQboTokens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CompanyId = table.Column<int>(type: "INTEGER", nullable: false),
+                    RealmId = table.Column<string>(type: "TEXT", nullable: true),
+                    AccessToken = table.Column<string>(type: "TEXT", nullable: false),
+                    RefreshToken = table.Column<string>(type: "TEXT", nullable: false),
+                    TokenType = table.Column<string>(type: "TEXT", nullable: true),
+                    Scope = table.Column<string>(type: "TEXT", nullable: true),
+                    ExpiresAtUtc = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PayrollQboTokens", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PayrollQboTokens");
+        }
+    }
+}
